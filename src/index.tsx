1ead5981c95354582ac78dd408dbd4d53c9a3240
PaceMak1r <hashpgone@gmail.com>
2021-10-15 22:30:02 +0800

    7-9 改成正规的TS+redux结合的代码

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,13 @@ import './index.css';
 import App from './App';
 import 'antd/dist/antd.css'
 import './i18n/configs'//因为在引用i18n会自动将这个API注入
+import {Provider} from 'react-redux'
+import store from './redux/store'
 ReactDOM.render(
   <React.StrictMode>
+ <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,4 +2,6 @@ import { createStore } from 'redux'
 import languageReducer from './language/languageReducer';
 const store = createStore(languageReducer);
 
+export type RooutState = ReturnType<typeof store.getState>
+
 export default store
